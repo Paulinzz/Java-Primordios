d516bc1b5423a245ad0876380d9cc71fc1588ae9
paulinzz <paulofernandesalves30@gmail.com>
2025-08-18 23:34:21 -0300

    primeiras impressões java - utilizando o openjdk-17

---
+++ Maratona-Java/src/academy/devdojo/maratona/introducao/Aula01_HelloWorld.java
@@ -0,0 +1,20 @@
+// toda classe dentro do pacote, tera que ter esse "package"
+
+package academy.devdojo.maratona.introducao;
+
+// cometarios:
+
+// <-- cometario de uma linha
+/*
+    cometarios em bloco
+ */
+
+/**
+ *  Isso é um cometario  <b>javadoc<b/>
+ */
+
+public class Aula01_HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
---
+++ Maratona-Java/src/academy/devdojo/maratona/introducao/Aula02_TiposPrimitivos.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratona.introducao;
+
+public class Aula02_TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = -128;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'A';
+
+        System.out.println("A idade é "+idade+" anos");
+        System.out.println(falso);
+        System.out.println("char "+caractere);
+    }
+}
